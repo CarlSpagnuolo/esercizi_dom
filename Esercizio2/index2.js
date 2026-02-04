a3f9e9e7fcb64083a213a0b9eb0308f36676735d
Carlo Spagnuolo <carloppannolo@hotmail.it>
2026-02-04 21:50:33 +0100

    es-2

---
+++ Esercizio2/index2.js
@@ -0,0 +1,12 @@
+const p = document.querySelectorAll(".paragrafo");
+p.forEach((el) => {
+  console.log(el.innerText);
+});
+
+const li = document.querySelector("#elemento-3");
+console.log(li.innerText);
+
+const h2 = document.querySelectorAll("h2");
+h2.forEach((el) => {
+  console.log(el.innerText);
+});
